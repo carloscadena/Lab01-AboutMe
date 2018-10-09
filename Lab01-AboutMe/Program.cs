c7a5202a47ae040b5e190337a72dce99752a59da
carlos cadena <carloscadena@live.com>
2018-10-09 10:14:57 -0700

    First question done without error handling

--- Lab01-AboutMe/Program.cs
+++ Lab01-AboutMe/Program.cs
@@ -6,7 +6,23 @@ namespace Lab01_AboutMe
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to the quiz about Carlos. This quiz will consist of 5 questions. When you're ready hit enter.");
+            Console.ReadLine();
+            QuestionOne();
+        }
+        static void QuestionOne()
+        {
+            Console.WriteLine("On what day in November is my birthday?");
+            int input = int.Parse(Console.ReadLine());
+            if(input == 4)
+            {
+                Console.WriteLine("An amazing guess! You got it.");
+            }
+            else
+            {
+                Console.WriteLine("I guess you don't know me at all. Did you ever care?");
+            }
+
         }
     }
 }
